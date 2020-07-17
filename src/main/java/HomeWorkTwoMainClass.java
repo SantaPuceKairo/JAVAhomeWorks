4aadde827ddb493c53e263cc451117e1ed78135f
SantaPuceKairo <santa.puce@gmail.com>
2020-07-17 11:13:19 +0300

    HomeWork 2 - HashMAP - changed file

--- src/main/java/HomeWorkTwoMainClass.java
+++ src/main/java/HomeWorkTwoMainClass.java
@@ -8,66 +8,53 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HomeWorkTwoMainClass {
-           public static void main(String[] args) {
-            String text = "It is much more difficult to judge oneself than to judge others. If you succeed in judging yourself rightly, then you are indeed a man of true wisdom. Very very very true message.";
-            String[] words = text.split(" ");
-            System.out.println("GIVEN TEXT: " + text);
-            System.out.println(" ");
-            System.out.println("*** Count of all words in the given text is: " + words.length);
+    public static void main(String[] args) {
+        String text = "It is much more difficult to judge oneself than to judge others. If you succeed in judging yourself rightly, then you are indeed a man of true wisdom. Very very very true message.";
+        System.out.println("1) ORIGINAL GIVEN TEXT: " + text);
+        //Get text in lowerCase, remove dots and comma
+        text = text.toLowerCase().replaceAll("  ", "").replace(".", "").replace(",", "");
+        System.out.println(" ");
+        System.out.println("2) TEXT WITH REMOVED CHARS + IN LOWERCASE: " + text);
 
 
-            HashMap<String, Integer> uniqueWords = new HashMap<>();
-            uniqueWords.put("It", 1);
-            uniqueWords.put("is", 1);
-            uniqueWords.put("much", 1);
-            uniqueWords.put("more", 1);
-            uniqueWords.put("difficult", 1);
-            uniqueWords.put("to", 1);
-            uniqueWords.put("judge", 1);
-            uniqueWords.put("oneself", 1);
-            uniqueWords.put("than", 1);
-            uniqueWords.put("to", 2);
-            uniqueWords.put("judge", 2);
-            uniqueWords.put("others", 1);
-            uniqueWords.put("If", 1);
-            uniqueWords.put("you", 1);
-            uniqueWords.put("succeed", 1);
-            uniqueWords.put("in", 1);
-            uniqueWords.put("judging", 1);
-            uniqueWords.put("yourself", 1);
-            uniqueWords.put("rightly", 1);
-            uniqueWords.put("then", 1);
-            uniqueWords.put("you", 2);
-            uniqueWords.put("are", 1);
-            uniqueWords.put("indeed", 1);
-            uniqueWords.put("a", 1);
-            uniqueWords.put("man", 1);
-            uniqueWords.put("of", 1);
-            uniqueWords.put("true", 1);
-            uniqueWords.put("wisdom", 1);
-            uniqueWords.put("very", 1);
-            uniqueWords.put("very", 2);
-            uniqueWords.put("very", 3);
-            uniqueWords.put("true", 2);
-            uniqueWords.put("message", 1);
+//split text in words
+        System.out.println(" ");
+        String[] words = text.split(" ");
+        System.out.println("3) COUNT OF WORDS IS: " + words.length);
 
+//Hashmap
 
-            System.out.println("***List of unique words in the given text:");
-            for (String i : uniqueWords.keySet()) {
-                System.out.println(i);
+        Map<String, Integer> uniquewords = new HashMap<String, Integer>();
+
+        for (int i=0; i<words.length ; i++) {
+
+            //if word exists in the text, add 1 to the HashMap value
+            //words are taken from the massive
+
+            if (uniquewords.containsKey(words[i])) {
+                int value = uniquewords.get(words[i]);
+                uniquewords.put(words[i], value + 1);
             }
-            System.out.println(" ");
-            System.out.println("*** Count of unique words in the given text is: " + uniqueWords.size());
+            //otherwise use 1 for the value if the word is unique
+            else {
 
-            System.out.println(" ");
-            System.out.println("*** Count of each word used in the given text");
-
-            for (Map.Entry<String, Integer> entry : uniqueWords.entrySet()) {
-                System.out.println("\t Word: " + entry.getKey() + "\t\t\t\tCount: " + entry.getValue());
-
+                uniquewords.put(words[i], 1);
             }
-
         }
+        System.out.println(" ");
+        System.out.println("4) HASHMAP value / word = Count of used times: ");
+        System.out.println(uniquewords);
+        System.out.println(" ");
+        System.out.println("5) COUNT OF UNIQUE WORDS: " + uniquewords.size());
     }
+}
+
+
+
+
+
+
+
+
 
 
